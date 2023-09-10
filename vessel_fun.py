5ec1dc9d9a9c433690495a2753240ca118f79d90
Hipnopotam <137421723+Hipnopotam@users.noreply.github.com>
2023-09-10 15:00:20 +0300

    res4

--- vessel_fun.py
+++ vessel_fun.py
@@ -43,69 +43,72 @@ def result_fun(message):
     ntd, ntdMess = resultNTD
     t=f'Параметры сосуда:\nРабочее давление - {rabD} МПа, \nРабочая температура - {rabT} С,\nОбъем сосуда - {obem} м3, \nСреда - {sreda}\nПроизведение давления на вместимость - {str(round(rabD*obem,5))}.'
     result =  t+'\n\n'+ntdMess
+    zapros2 = f"UPDATE ras4et set ntd='{ntd}' WHERE telegram_id={message.chat.id}"
+    db.execute_query(connection, zapros2, 'НТД внесение в БД')
+    connection.close()
     return result
 
 
-def ntd_fun(message):
-    obem = str(message.text)
-    if fun.check_digit(obem)==False:
-        return None
-    else:
-        obem=float(fun.zapyataya(obem))
-        #работа с БД
-        connection = db.create_connection("ORPD.sqlite",'4')
-        zapros=f'UPDATE ras4et SET obem={obem} WHERE telegram_id={message.chat.id}'
-        db.execute_query(connection, zapros, 'объем внесение в БД')
+# def ntd_fun(message):
+#     obem = str(message.text)
+#     if fun.check_digit(obem)==False:
+#         return None
+#     else:
+#         obem=float(fun.zapyataya(obem))
+#         #работа с БД
+#         connection = db.create_connection("ORPD.sqlite",'4')
+#         zapros=f'UPDATE ras4et SET obem={obem} WHERE telegram_id={message.chat.id}'
+#         db.execute_query(connection, zapros, 'объем внесение в БД')
 
-        #Запрос из БД-таблицы ras4et - рабочего давления, среда, темп кипения
-        zapros1=f'SELECT rabD, sreda, tKip, rabT, prime4anie FROM ras4et WHERE telegram_id={message.chat.id}'
-        zap=db.execute_read_query(connection,zapros1,'Запрос')
-        header=['рабД','среда','Ткипен', 'РабТ', 'подписка']
-        print(tabulate(zap, headers=header, tablefmt='grid'))
-        rabD=float(zap[0][0])
-        sreda=zap[0][1]
-        tKip=float(zap[0][2])
-        rabT=float(zap[0][3])
-        podpiska=zap[0][4]
+#         #Запрос из БД-таблицы ras4et - рабочего давления, среда, темп кипения
+#         zapros1=f'SELECT rabD, sreda, tKip, rabT, prime4anie FROM ras4et WHERE telegram_id={message.chat.id}'
+#         zap=db.execute_read_query(connection,zapros1,'Запрос')
+#         header=['рабД','среда','Ткипен', 'РабТ', 'подписка']
+#         print(tabulate(zap, headers=header, tablefmt='grid'))
+#         rabD=float(zap[0][0])
+#         sreda=zap[0][1]
+#         tKip=float(zap[0][2])
+#         rabT=float(zap[0][3])
+#         podpiska=zap[0][4]
 
-        resultNTD=ntd_fun_result(rabD,obem,rabT,tKip,sreda)
-        ntdMess=resultNTD[1]
-        # print('ntdMess - ',ntdMess)
-        ntd=resultNTD[0]
+#         resultNTD=ntd_fun_result(rabD,obem,rabT,tKip,sreda)
+#         ntdMess=resultNTD[1]
+#         # print('ntdMess - ',ntdMess)
+#         ntd=resultNTD[0]
 
-        t=f'Параметры сосуда:\nРабочее давление - {rabD} МПа, \nРабочая температура - {rabT} С,\nОбъем сосуда - {obem} м3, \nСреда - {sreda}\nПроизведение давления на вместимость - {str(round(rabD*obem,5))}.'
-        result =  t+'\n\n'+ntdMess
+#         t=f'Параметры сосуда:\nРабочее давление - {rabD} МПа, \nРабочая температура - {rabT} С,\nОбъем сосуда - {obem} м3, \nСреда - {sreda}\nПроизведение давления на вместимость - {str(round(rabD*obem,5))}.'
+#         result =  t+'\n\n'+ntdMess
 
-        zapros=f"UPDATE ras4et SET ntd='{ntd}' WHERE telegram_id={message.chat.id}"
-        db.execute_query(connection, zapros, 'НТД внесение в БД')
-        connection.close()
-        #конец работы с БД
+#         zapros=f"UPDATE ras4et SET ntd='{ntd}' WHERE telegram_id={message.chat.id}"
+#         db.execute_query(connection, zapros, 'НТД внесение в БД')
+#         connection.close()
+#         #конец работы с БД
 
-        if podpiska=='no_active':
-            result += '\n\nНа этом обзор демо версии окончен. В полной версии вы можете рассчитать периодичность НВО и ГИ, необходимость постановки на учет в РТН.\n\n Для просмотра главного меню введите /start\n\nДля расчета сосудов введите /vessel\n\nДля расчета трубопроводов введите /pipe\n\nДля активации введите /activate'
-            return result
+#         if podpiska=='no_active':
+#             result += '\n\nНа этом обзор демо версии окончен. В полной версии вы можете рассчитать периодичность НВО и ГИ, необходимость постановки на учет в РТН.\n\n Для просмотра главного меню введите /start\n\nДля расчета сосудов введите /vessel\n\nДля расчета трубопроводов введите /pipe\n\nДля активации введите /activate'
+#             return result
         
 
-        if ntd!='ФНП' and podpiska!='no_active': #Дописать проверку на активную подписку
-            result_fnp = fnp_fun(ntd,0,rabT,rabD,obem)
-            # result_ntd = ntd_fun_result(rabD,obem,rabT,tKip,sreda)
-            # r=bot.send_message(message.chat.id, result[1], parse_mode='html')
-            #работа с БД
-            connection = db.create_connection("ORPD.sqlite",'5')
-            zapros=f"UPDATE ras4et SET rtn='0' WHERE telegram_id={message.chat.id}"
-            db.execute_query(connection, zapros, 'РТН внесение в БД')
-            connection.close()
-            #конец работы с БД
+#         if ntd!='ФНП' and podpiska!='no_active': #Дописать проверку на активную подписку
+#             result_fnp = fnp_fun(ntd,0,rabT,rabD,obem)
+#             # result_ntd = ntd_fun_result(rabD,obem,rabT,tKip,sreda)
+#             # r=bot.send_message(message.chat.id, result[1], parse_mode='html')
+#             #работа с БД
+#             connection = db.create_connection("ORPD.sqlite",'5')
+#             zapros=f"UPDATE ras4et SET rtn='0' WHERE telegram_id={message.chat.id}"
+#             db.execute_query(connection, zapros, 'РТН внесение в БД')
+#             connection.close()
+#             #конец работы с БД
 
-            result += f"\n\n{result_fnp}"
-            # print(result, ' -  result')
-            return result
+#             result += f"\n\n{result_fnp}"
+#             # print(result, ' -  result')
+#             return result
 
-        elif ntd=='ФНП' and podpiska!='no_active':
-            rtnMess='\n\nДля проверки необходимости постановки на учет в РТН введите группу среды по ТР ТС 032:'
+#         elif ntd=='ФНП' and podpiska!='no_active':
+#             rtnMess='\n\nДля проверки необходимости постановки на учет в РТН введите группу среды по ТР ТС 032:'
             
-        result += rtnMess
-        return result
+#         result += rtnMess
+#         return result
 
 
 def ntd_fun_result(rabD,obem,rabT,tKip,sreda):
@@ -162,16 +165,16 @@ def rtn_fun(message):
     #конец работы с БД
     return rtnMess
 
-# +++++++++++++++++++++++++++++++++++++++++++++
-def sos_type_fun(message):
-    sosType=str(message.text)
-    #работа с БД
-    connection = db.create_connection("ORPD.sqlite", '8')
-    zapros=f"UPDATE ras4et SET sosType='{sosType}' WHERE telegram_id={message.chat.id}"
-    db.execute_query(connection, zapros, 'Тип сосуда внесение в БД')
-    connection.close()
-    #конец работы с БД
-    return True
+# # +++++++++++++++++++++++++++++++++++++++++++++
+# def sos_type_fun(message):
+#     sosType=str(message.text)
+#     #работа с БД
+#     connection = db.create_connection("ORPD.sqlite", '8')
+#     zapros=f"UPDATE ras4et SET sosType='{sosType}' WHERE telegram_id={message.chat.id}"
+#     db.execute_query(connection, zapros, 'Тип сосуда внесение в БД')
+#     connection.close()
+#     #конец работы с БД
+#     return True
 
 
 
@@ -193,61 +196,61 @@ def fnp_fun(ntd,srTRTS,rabT,rabD,obem):
     result=rtnMess
     return result
 
-def srok_fun(message, warning='ok'):
-    srok = str(message.text)
-    print(srok)
-    if fun.check_digit(srok) == False:
-        return None
-    else:
-        srok = float(fun.zapyataya(srok))
-        #работа с БД
-        connection = db.create_connection("ORPD.sqlite", '7')
-        zapros = f"UPDATE ras4et SET srok='{srok}' WHERE telegram_id={message.chat.id}"
-        db.execute_query(connection, zapros, 'Срок внесение в БД')
-        connection.close()
-        #конец работы с БД
-        return warning
+# def srok_fun(message, warning='ok'):
+#     srok = str(message.text)
+#     print(srok)
+#     if fun.check_digit(srok) == False:
+#         return None
+#     else:
+#         srok = float(fun.zapyataya(srok))
+#         #работа с БД
+#         connection = db.create_connection("ORPD.sqlite", '7')
+#         zapros = f"UPDATE ras4et SET srok='{srok}' WHERE telegram_id={message.chat.id}"
+#         db.execute_query(connection, zapros, 'Срок внесение в БД')
+#         connection.close()
+#         #конец работы с БД
+#         return warning
     
 
-def pribavka_fun(message):
-    connection = db.create_connection("ORPD.sqlite", '9')
-    zapros2=f'SELECT ntd, rtn, sosType, srok FROM ras4et WHERE telegram_id={message.chat.id}'
-    zap=db.execute_read_query(connection,zapros2,'Запрос')
-    header=['НТД', 'РТН', 'тип сосуда', 'срок']
+# def pribavka_fun(message):
+#     connection = db.create_connection("ORPD.sqlite", '9')
+#     zapros2=f'SELECT ntd, rtn, sosType, srok FROM ras4et WHERE telegram_id={message.chat.id}'
+#     zap=db.execute_read_query(connection,zapros2,'Запрос')
+#     header=['НТД', 'РТН', 'тип сосуда', 'срок']
     
-    print(tabulate(zap, headers=header, tablefmt='grid'))
-    ntd=zap[0][0]
-    rtn=int(zap[0][1])
-    sosType=zap[0][2]
-    srok=float(zap[0][3])
-    pribavka=str(message.text)
-    if fun.check_digit(pribavka)==False:
-        return None
-    else:
-        pribavka=float(fun.zapyataya(pribavka))
-        skKorr=round(pribavka/srok,4)
+#     print(tabulate(zap, headers=header, tablefmt='grid'))
+#     ntd=zap[0][0]
+#     rtn=int(zap[0][1])
+#     sosType=zap[0][2]
+#     srok=float(zap[0][3])
+#     pribavka=str(message.text)
+#     if fun.check_digit(pribavka)==False:
+#         return None
+#     else:
+#         pribavka=float(fun.zapyataya(pribavka))
+#         skKorr=round(pribavka/srok,4)
 
-        result = f'Расчетная скорость коррозии: {skKorr} мм/год'
-        #работа с БД
-        zapros=f"UPDATE ras4et SET pribavka='{pribavka}', skKorr='{skKorr}' WHERE telegram_id={message.chat.id}"
-        db.execute_query(connection, zapros, 'Прибавка и ск. корр. внесение в БД')
+#         result = f'Расчетная скорость коррозии: {skKorr} мм/год'
+#         #работа с БД
+#         zapros=f"UPDATE ras4et SET pribavka='{pribavka}', skKorr='{skKorr}' WHERE telegram_id={message.chat.id}"
+#         db.execute_query(connection, zapros, 'Прибавка и ск. корр. внесение в БД')
         
-        #вывод таблицы ras4et
-        getColumnNames=connection.execute("select * from ras4et limit 1")
-        colName=[i[0] for i in getColumnNames.description]
+#         #вывод таблицы ras4et
+#         getColumnNames=connection.execute("select * from ras4et limit 1")
+#         colName=[i[0] for i in getColumnNames.description]
 
-        vivod=f'SELECT * FROM ras4et WHERE telegram_id={message.chat.id}'
-        viv=db.execute_read_query(connection, vivod)
-        print(tabulate(viv, headers=colName, tablefmt='grid'))
+#         vivod=f'SELECT * FROM ras4et WHERE telegram_id={message.chat.id}'
+#         viv=db.execute_read_query(connection, vivod)
+#         print(tabulate(viv, headers=colName, tablefmt='grid'))
         
-        #конец работы с БД
+#         #конец работы с БД
         
-        resultRevizia=revizia_fun(ntd,rtn,skKorr,sosType)
+#         resultRevizia=revizia_fun(ntd,rtn,skKorr,sosType)
 
-        result += '\n\n'+resultRevizia
-    connection.close()
-    result += '\n\n\n' + 'Для просмотра главного меню введите /start\n\nДля расчета сосудов введите /vessel\n\nдля расчета трубопроводов введите /pipe'
-    return result
+#         result += '\n\n'+resultRevizia
+#     connection.close()
+#     result += '\n\n\n' + 'Для просмотра главного меню введите /start\n\nДля расчета сосудов введите /vessel\n\nдля расчета трубопроводов введите /pipe'
+#     return result
 
 
 
@@ -269,7 +272,7 @@ def revizia_fun(message):
     connection = db.create_connection("ORPD.sqlite", '9')
     zapros2=f'SELECT ntd, rtn, sosType, skKorr FROM ras4et WHERE telegram_id={message.chat.id}'
     zap=db.execute_read_query(connection,zapros2,'Запрос')
-    header=['НТД', 'РТН', 'тип сосуда', 'срок']
+    header=['НТД', 'РТН', 'тип сосуда', 'скорость коррозии']
     
     print(tabulate(zap, headers=header, tablefmt='grid'))
     ntd=zap[0][0]
@@ -324,5 +327,6 @@ def revizia_fun(message):
     elif ntd=='ИТНЭ' and skKorr>0.3 and sosType=='Теплообменник':
         resultRevizia=f'{ntd}-93, Раздел 2, таблица 2\nДля теплообменников при скорости коррозии {skKorr}>0.3 мм/год\nНВО - раз в {nvoRUA03[4]}, ГИ - раз в {nvoRUA03[5]}'
     else:
+        resultRevizia = None
         print('ошибка 3')
     return resultRevizia
