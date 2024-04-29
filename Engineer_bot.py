1ca1d8cf644a079783065fc4851bc76615c43e86
Hipnopotam <137421723+Hipnopotam@users.noreply.github.com>
2024-04-29 14:11:31 +0300

    title row 1

--- Engineer_bot.py
+++ Engineer_bot.py
@@ -182,7 +182,7 @@ def final_document(message):
                 # получаем ячейку таблицы
                 cell = table.cell(row, col)
                 # записываем в ячейку данные
-                index = i - row - 1
+                index = i - row #i = 4 , row = 1 -> index = 3
                 cell.text = str(all_user_defects_list[index])
             elif col == 2:
                 # получаем ячейку таблицы
