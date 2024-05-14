c16d7f3bbe700d8e265ad6a550d87a839ea98c42
Hipnopotam <137421723+Hipnopotam@users.noreply.github.com>
2024-05-14 14:33:18 +0300

    8

--- Engineer_bot.py
+++ Engineer_bot.py
@@ -157,7 +157,7 @@ def final_document(message):
 Начальник {message.text} - \t\t\t\t
 Механик {message.text} - 
 '''
-        doc.add_heading(title, 0)
+        doc.add_heading(title, 0, alignment = 'center')
         doc.add_heading(title_1,2)
         doc.add_paragraph(comission)
         # добавляем таблицу 3x3
